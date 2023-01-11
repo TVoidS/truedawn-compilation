5fdd5f53e096c1d77243555aca08f02404dce35a
TVoidS <werme3@gmail.com>
2023-01-10 19:05:22 -0500

    Player Camera Done! Until I don't like it or add a new game mode

---
+++ Assets/MoveCamera.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour
+{
+    public Transform cameraPosition;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = cameraPosition.position;
+    }
+}
---
+++ Assets/PlayerCam.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCam : MonoBehaviour
+{
+    public float senseX;
+    public float senseY;
+
+    public Transform orientation;
+
+    float xRotation;
+    float yRotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * senseX;
+        float mouseY = -Input.GetAxisRaw("Mouse Y") * Time.deltaTime * senseY;
+
+        xRotation += mouseX;
+        yRotation += mouseY;
+
+        yRotation = Mathf.Clamp(yRotation, -90f, 90f);
+
+        transform.rotation = Quaternion.Euler(yRotation, xRotation, 0);
+        orientation.rotation = Quaternion.Euler(0, xRotation, 0);
+    }
+}
