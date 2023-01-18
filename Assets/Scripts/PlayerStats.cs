77041f7bc7664b377c39b85024f97d0e0a8d78ef
TVoidS <werme3@gmail.com>
2023-01-17 19:14:41 -0500

    Still working on the UI, but transfering the skill types to an enumerator class.

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -1,15 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerStats : MonoBehaviour
 {
 
     //TODO: Make this the entire character stat and skill screen for all games. :D
 
+    [Header("Spirit Vein UI Button Objects")]
+    public Button test;
+
     // Start is called before the first frame update
     void Start()
     {
+        // TODO: Automatically form a collection of buttons that meet a certain criteiria (have a specific component attatched)
+        // That list will be used for connecting to skills or anything else, based on the ID of the attatched component
+        Button testBtn = test.GetComponent<Button>();
+        testBtn.onClick.AddListener(TestPrint);
+    }
+
+    void TestPrint() 
+    {
+        Debug.Log("Test Success");
         
     }
 
--- Assets/Scripts/Standard Movement/PlayerCam.cs
+++ Assets/Scripts/Standard Movement/PlayerCam.cs
@@ -17,7 +17,6 @@ public class PlayerCam : MonoBehaviour
     {
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
-
     }
 
     // Update is called once per frame
