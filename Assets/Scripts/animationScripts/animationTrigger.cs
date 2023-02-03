b3b09e34deb1610a662a80496c85f3585bfbcda5
RoxBlox <eskoch77@gmail.com>
2023-02-03 21:52:54 +0100

    started animation script

---
+++ Assets/Scripts/animationScripts/animationTrigger.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class animationTrigger : MonoBehaviour {
+    public Animator anim;
+    void Start () {
+        anim = GetComponent<Animator>();
+    }
+    void Update () {
+        if (Input.GetKeyDown("1"))
+        {
+            anim.Play("startRotation");
+            anim.Play("loopRotation");
+        }
+    }
+};
